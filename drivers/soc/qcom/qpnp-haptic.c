LineageOS/android_kernel_motorola_msm8998

soc: qcom: haptic: Correct return value of show() methods
By convention, these methods should return the number of bytes
actually written into the buffer, not the number of bytes that
would have been written.

Change-Id: Iab8b292397bf3cbbf4a59a72812d3831ac7722c4
Signed-off-by: Zhao Wei Liew <zhaoweiliew@gmail.com>

drivers/soc/qcom/qpnp-haptic.c
@@ -1800,19 +1800,19 @@ static ssize_t qpnp_hap_vmax_default(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vmax_default_mv);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vmax_default_mv);
 }
 
 static ssize_t qpnp_hap_vmax_max(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "%d\n", QPNP_HAP_VMAX_MAX_MV);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", QPNP_HAP_VMAX_MAX_MV);
 }
 
 static ssize_t qpnp_hap_vmax_min(struct device *dev,
 		struct device_attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "%d\n", QPNP_HAP_VMAX_MIN_MV);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", QPNP_HAP_VMAX_MIN_MV);
 }
 
 /* sysfs attributes */

bjakp12/kernel_xiaomi

Signed-off-by: sohamxda7 <sensoham135@gmail.com>
Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
Signed-off-by: clarencelol <clarencekuiek@icloud.com>
Signed-off-by: pix106 <sbordenave@gmail.com>

drivers/soc/qcom/qpnp-haptic.c
@@ -1805,7 +1805,7 @@ static ssize_t qpnp_hap_min_show(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_min);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_min);
 }
 
 static ssize_t qpnp_hap_max_show(struct device *dev,
@@ -1815,7 +1815,7 @@ static ssize_t qpnp_hap_max_show(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_max);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_max);
 }
 
 static ssize_t qpnp_hap_default_show(struct device *dev,
@@ -1825,7 +1825,7 @@ static ssize_t qpnp_hap_default_show(struct device *dev,
 	struct qpnp_hap *hap = container_of(timed_dev, struct qpnp_hap,
 					 timed_dev);
 
-	return snprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_default);
+	return scnprintf(buf, PAGE_SIZE, "%d\n", hap->vtg_default);
 }
 
 /* sysfs attributes */